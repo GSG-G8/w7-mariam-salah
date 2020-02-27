1f908ee35411ff2ed9a0df93cf6fe4bbaefdc7ef
ahmadysalah <a.ahmad.salah@gmail.com>
2020-02-27 15:40:04 +0200

    fix post #25

--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,7 +1,8 @@
 const connection = require('../config/connection');
 
 const getpost = () => connection.query('select * from posts');
-const createpost = (data) => connection.query('INSERT INTO posts(title,detail,img) VALUES ($1,$2,$3)',
-  [data.title, data.detail, data.img]);
+const createpost = (data) => connection
+  .query('INSERT INTO posts(title,detail,img) VALUES ($1,$2,$3)',
+    [data.title, data.detail, data.img]);
 
 module.exports = { getpost, createpost };
