550dda521c751e144e0f711046f6bf90da2a5eec
ahmadysalah <a.ahmad.salah@gmail.com>
2020-02-29 09:34:10 +0200

    working on issues 31,32,33,34,35,37

--- server/database/config/build.js
+++ server/database/config/build.js
@@ -4,13 +4,12 @@ const { readFileSync } = require('fs');
 const connection = require('./connection');
 
 const sql = readFileSync(join(__dirname, 'build.sql')).toString();
-const build = () => {
-  connection
-    .query(sql)
-    .then(() => {
-      console.log('create tables done successfully');
-    }).catch((err) => {
-      console.error('eror', err);
-    });
-};
+const build = () => connection
+  .query(sql)
+  .then(() => {
+    console.log('create tables done successfully');
+  }).catch((err) => {
+    console.error('eror', err);
+  });
+
 module.exports = build;
